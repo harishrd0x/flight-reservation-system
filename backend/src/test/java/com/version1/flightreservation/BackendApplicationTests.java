2b11eaaeb1c19bc38947fcdc79ef6dd325ae73c5
harishrd0x <harishrd321@gmail.com>
2025-04-09 23:51:29 +0530

    add backend boilerplate

---
+++ backend/src/test/java/com/version1/flightreservation/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.version1.flightreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c2087232ddc65bade5312ffe15a3e21d97e73aad
Harish <73774584+harishrd0x@users.noreply.github.com>
2025-04-10 21:35:25 +0530

    Update AirplaneService.java

--- backend/src/main/java/com/version1/frs/service/AirplaneService.java
+++ backend/src/main/java/com/version1/frs/service/AirplaneService.java
@@ -1,5 +1,37 @@
 package com.version1.frs.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.version1.frs.model.Airplane;
+import com.version1.frs.repository.AirplaneRepository;
+
+@Service
 public class AirplaneService {
+	
+  private AirplaneRepository airplaneRepository;
+    @Autowired
+	public AirplaneService(AirplaneRepository airplaneRepository) {
+		this.airplaneRepository = airplaneRepository;
+	}
+    
+	public String addAirplane(Airplane airplane) {
+		
+		airplaneRepository.save(airplane);
+		return "Airplane added successfully";
+	}
+	
+	public String deleteAirplane(Long id) {
+		airplaneRepository.deleteById(id);
+		return "Airplane deleted successfully";
+	}
+	
+	public Iterable<Airplane> getAllAirplanes() {
+		return airplaneRepository.findAll();
+	}
+	
+	public Airplane getAirplaneById(Long id) {
+		return airplaneRepository.findById(id).orElse(null);
+	}
 
 }

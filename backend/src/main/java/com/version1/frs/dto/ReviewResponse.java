1fdb048ac507f3e7a66f4c07b7df254ecaede37a
unknown <subhenduadak54@gmail.com>
2025-04-22 10:40:49 +0100

    Add JavaDoc comments for DTO classes

--- backend/src/main/java/com/version1/frs/dto/ReviewResponse.java
+++ backend/src/main/java/com/version1/frs/dto/ReviewResponse.java
@@ -1,51 +1,123 @@
+/*
+ * Copyright 2022-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.version1.frs.dto;
 
+/**
+ * DTO for the review response. Provides the details of a review submitted by a
+ * user for a flight.
+ * 
+ * @author YourName
+ * @since 1.0
+ */
 public class ReviewResponse {
 
-    private Long reviewId;
-    private Long userId;
-    private Long flightId;
-    private Float rating;
-    private String reviewText;
+	private Long reviewId;
+	private Long userId;
+	private Long flightId;
+	private Float rating;
+	private String reviewText;
 
-    // Getters and Setters
-    public Long getReviewId() {
-        return reviewId;
-    }
+	/**
+	 * Gets the review ID.
+	 * 
+	 * @return review ID
+	 */
+	public Long getReviewId() {
+		return reviewId;
+	}
 
-    public void setReviewId(Long reviewId) {
-        this.reviewId = reviewId;
-    }
+	/**
+	 * Sets the review ID.
+	 * 
+	 * @param reviewId the review ID
+	 */
+	public void setReviewId(Long reviewId) {
+		this.reviewId = reviewId;
+	}
 
-    public Long getUserId() {
-        return userId;
-    }
+	/**
+	 * Gets the user ID.
+	 * 
+	 * @return user ID
+	 */
+	public Long getUserId() {
+		return userId;
+	}
 
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
+	/**
+	 * Sets the user ID.
+	 * 
+	 * @param userId the user ID
+	 */
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
 
-    public Long getFlightId() {
-        return flightId;
-    }
+	/**
+	 * Gets the flight ID.
+	 * 
+	 * @return flight ID
+	 */
+	public Long getFlightId() {
+		return flightId;
+	}
 
-    public void setFlightId(Long flightId) {
-        this.flightId = flightId;
-    }
+	/**
+	 * Sets the flight ID.
+	 * 
+	 * @param flightId the flight ID
+	 */
+	public void setFlightId(Long flightId) {
+		this.flightId = flightId;
+	}
 
-    public Float getRating() {
-        return rating;
-    }
+	/**
+	 * Gets the review rating.
+	 * 
+	 * @return rating
+	 */
+	public Float getRating() {
+		return rating;
+	}
 
-    public void setRating(Float rating) {
-        this.rating = rating;
-    }
+	/**
+	 * Sets the review rating.
+	 * 
+	 * @param rating the rating value
+	 */
+	public void setRating(Float rating) {
+		this.rating = rating;
+	}
 
-    public String getReviewText() {
-        return reviewText;
-    }
+	/**
+	 * Gets the review text.
+	 * 
+	 * @return review text
+	 */
+	public String getReviewText() {
+		return reviewText;
+	}
 
-    public void setReviewText(String reviewText) {
-        this.reviewText = reviewText;
-    }
+	/**
+	 * Sets the review text.
+	 * 
+	 * @param reviewText the review text
+	 */
+	public void setReviewText(String reviewText) {
+		this.reviewText = reviewText;
+	}
 }
--- backend/src/main/java/com/version1/frs/dto/WalletResponse.java
+++ backend/src/main/java/com/version1/frs/dto/WalletResponse.java
@@ -1,34 +1,97 @@
+/*
+ * Copyright 2002-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.version1.frs.dto;
 
 import java.math.BigDecimal;
 
+/**
+ * Data Transfer Object for Wallet Response. This class is used to represent the
+ * details of a wallet.
+ */
 public class WalletResponse {
 
-    private Long walletId;
-    private Long userId;
-    private BigDecimal  balance;
+	/**
+	 * The unique identifier for the wallet.
+	 */
+	private Long walletId;
 
-    public Long getWalletId() {
-        return walletId;
-    }
+	/**
+	 * The ID of the user who owns the wallet.
+	 */
+	private Long userId;
 
-    public void setWalletId(Long walletId) {
-        this.walletId = walletId;
-    }
+	/**
+	 * The balance in the wallet.
+	 */
+	private BigDecimal balance;
 
-    public Long getUserId() {
-        return userId;
-    }
+	// Getters and Setters
 
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
+	/**
+	 * Gets the wallet ID.
+	 * 
+	 * @return the wallet ID
+	 */
+	public Long getWalletId() {
+		return walletId;
+	}
 
-    public BigDecimal  getBalance() {
-        return balance;
-    }
+	/**
+	 * Sets the wallet ID.
+	 * 
+	 * @param walletId the wallet ID to set
+	 */
+	public void setWalletId(Long walletId) {
+		this.walletId = walletId;
+	}
 
-    public void setBalance(BigDecimal  balance) {
-        this.balance = balance;
-    }
+	/**
+	 * Gets the user ID.
+	 * 
+	 * @return the user ID
+	 */
+	public Long getUserId() {
+		return userId;
+	}
+
+	/**
+	 * Sets the user ID.
+	 * 
+	 * @param userId the user ID to set
+	 */
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * Gets the balance in the wallet.
+	 * 
+	 * @return the balance
+	 */
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	/**
+	 * Sets the balance in the wallet.
+	 * 
+	 * @param balance the balance to set
+	 */
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
 }
